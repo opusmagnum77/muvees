e3f2ce42d65d1902ed1815761941b67e0f980829
opusmagnum77 <daniel.adeyanju@gmail.com>
2016-05-03 14:48:03 -0400

    Create custom collection view cell with image iew and add button in cv's footer

---
+++ muvees/muvees/MVCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MVCollectionViewCell.h
+//  muvees
+//
+//  Created by Daniel Adeyanju on 5/3/16.
+//  Copyright © 2016 Daniel Adeyanju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MVCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ muvees/muvees/MVCollectionViewCell.m
@@ -0,0 +1,16 @@
+//
+//  MVCollectionViewCell.m
+//  muvees
+//
+//  Created by Daniel Adeyanju on 5/3/16.
+//  Copyright © 2016 Daniel Adeyanju. All rights reserved.
+//
+
+#import "MVCollectionViewCell.h"
+@interface MVCollectionViewCell()
+@property (weak, nonatomic) IBOutlet UIImageView *posterImageView;
+
+@end
+@implementation MVCollectionViewCell
+
+@end
